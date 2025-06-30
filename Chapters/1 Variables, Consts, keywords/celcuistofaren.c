caf53acb348e7b13891b2bba9ef41205017681d5
Rishit Garg <rishitalig@gmail.com>
2025-06-30 16:28:30 +0530

    Language Change

--- Chapters/1 Variables, Consts, keywords/celcuistofaren.c
+++ Chapters/1 Variables, Consts, keywords/celcuistofaren.c
@@ -3,7 +3,7 @@
 int main()
 {
     int celcius_temp;
-    printf("Input temperature in degree Celcius-");
+    printf("Input the temperature in degree Celcius-");
     scanf("%d", &celcius_temp);
 
     printf("Temperature is %f degree Farenheit", 1.8 * celcius_temp + 32);
