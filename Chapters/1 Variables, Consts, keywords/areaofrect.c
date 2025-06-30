c0cee6681738ab4941a710a720806cda035a575e
Rishit Garg <rishitalig@gmail.com>
2025-06-30 15:27:42 +0530

    Chater 3 done

--- Chapters/1 Variables, Consts, keywords/areaofrect.c
+++ Chapters/1 Variables, Consts, keywords/areaofrect.c
@@ -3,10 +3,10 @@
 int main()
 {
     int length, width;
-    printf("Enter length\n");
+    printf("Enter length of the rectangle \n");
     scanf("%d", &length);
 
-    printf("Enter width\n");
+    printf("Enter width of the rectangle \n");
     scanf("%d", &width);
 
     printf("area of this rectangle is %d", length * width);
