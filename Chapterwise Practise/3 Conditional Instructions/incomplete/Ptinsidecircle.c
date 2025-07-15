d1260f5ce462dd1b3ab02be85cdd0619f54c7ee6
Rishit Garg <rishitalig@gmail.com>
2025-07-15 14:25:32 +0530

    ...

--- Chapterwise Practise/3 Conditional Instructions/incomplete/Ptinsidecircle.c
+++
@@ -1,2 +0,0 @@
-// given the coordinates XY of center of a circle and its radius write a program that will determine whether a point lies inside the circle on the circle or outside the circle
-#include <stdio.h>
--- README.md
+++
@@ -1,4 +0,0 @@
-# Learning-C
-Author - Rishit Garg 
-<br>
-My journey as i learn C programming, includes all my projects and learnings 
