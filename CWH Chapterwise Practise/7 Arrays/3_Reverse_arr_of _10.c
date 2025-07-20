5cf8439f5432772b10443c947a426adb9a931175
Rishit Garg <rishitalig@gmail.com>
2025-07-21 02:09:53 +0530

    Arrays Day 1.2

---
+++ CWH Chapterwise Practise/7 Arrays/3_Reverse_arr_of _10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void reverse(int *ptr)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        int m = i, n = 9 - i, temp;
+
+        temp = ptr[m];
+        ptr[m] = ptr[n];
+        ptr[n] = temp;
+    }
+}
+
+
+int main() 
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *ptr = &arr;
+    reverse(ptr);
+
+// Printing To check
+
+    for (int j = 0; j < 10; j++)
+    {
+        printf("%d\n", arr[j]);
+    }
+
+    return 0;
+}
